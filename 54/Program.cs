1311d8e463950a93a2667488b54ff0059445ca66
muhammadkodir <muhammadkodyra@email.com>
2022-12-13 20:40:23 +0500

    final one

---
+++ 54/Program.cs
@@ -0,0 +1,53 @@
+﻿//  Задайте двумерный массив. Напишите программу,
+//  которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int[,] matrix = FillMatrix(3, 4);
+PrintMatrix(matrix);
+OrderArrayLines(matrix);
+Console.WriteLine();
+PrintMatrix(matrix);
+
+void OrderArrayLines(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+int[,] FillMatrix(int rows, int colums)
+{
+    int[,] matrix = new int[rows, colums];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 6);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}, ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ 58/Program.cs
@@ -0,0 +1,75 @@
+﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+
+int[,] matrix = NewMatrix(2, 2, 1, 10);
+PrintMatrix(matrix);
+System.Console.WriteLine();
+int[,] matrix2 = NewMatrix2(2, 2, 1, 10);
+PrintMatrix(matrix2);
+System.Console.WriteLine();
+int[,] result = ValueTwoMatrix(matrix, matrix2);
+PrintMatrix(result);
+
+int[,] ValueTwoMatrix(int[,] matrix, int[,] matrix2)
+{
+    var valueTwoMatrix = new int[matrix.GetLength(0), matrix2.GetLength(1)];
+
+    for (int i = 0; i < valueTwoMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < valueTwoMatrix.GetLength(1); j++)
+        {
+            valueTwoMatrix[i, j] = 0;
+            for (int k = 0; k < matrix.GetLength(1); k++)
+            {
+                valueTwoMatrix[i, j] += matrix[i, k] * matrix2[k, j];
+            }
+        }
+    }
+    return valueTwoMatrix;
+}
+
+int[,] NewMatrix(int rows, int columns, int minElement, int maxElement)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(minElement, maxElement + 1);
+        }
+    }
+    return matrix;
+}
+
+int[,] NewMatrix2(int rows, int columns, int minElement, int maxElement)
+{
+    int[,] matrix2 = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix2.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix2.GetLength(1); j++)
+        {
+            matrix2[i, j] = rnd.Next(minElement, maxElement + 1);
+        }
+    }
+    return matrix2;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1)
+                Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine("]");
+    }
+}
+
